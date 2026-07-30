cddc9eba61ed83bb17f7376bdece587af3f1d79e
wftijolosecologicos-hue <wftijolosecologicos@gmail.com>
2026-07-30 20:21:00 -0300

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='terralote-v2.0.0';
+const CACHE='terralote-v2-fix-20260730';
 const ASSETS=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
