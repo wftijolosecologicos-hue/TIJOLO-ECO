879d98b0db75c64e0f8abd6fa5de327d08dc0f24
wftijolosecologicos-hue <wftijolosecologicos@gmail.com>
2026-07-30 15:23:44 -0300

    Add files via upload

---
+++ frontend/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='terralote-v1';const ASSETS=['./','index.html','styles.css','app.js','manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('sync',e=>{if(e.tag==='terralote-sync')e.waitUntil(Promise.resolve())});
