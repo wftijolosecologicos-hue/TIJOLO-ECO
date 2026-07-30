583dd30dea994bddc3dc2cd69731c7161d619ca1
wftijolosecologicos-hue <wftijolosecologicos@gmail.com>
2026-07-30 20:29:20 -0300

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.TERRALOTE_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbx6Xg5GSZQJjdvI7XjXlg2YmATtWwu1gFF8yPKkRS-StCqWnn3FMPGXBSBzNheII8Ocnw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwkvE8KbJB8sabmG08wqzEGqVnSMyWbYi2PBz799vS6Q5HRdhDHM6BBgx9_xv4U6HRbJw/exec',
   APP_NAME: 'TerraLote',
   CACHE_VERSION: '2.0.0'
 });
