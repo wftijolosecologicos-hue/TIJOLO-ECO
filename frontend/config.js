0f78f9f03820cf92440c4eced61adcdf3e702a6f
wftijolosecologicos-hue <wftijolosecologicos@gmail.com>
2026-07-30 21:37:42 -0300

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,5 +0,0 @@
-window.TERRALOTE_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbyyWUk38PxBi7ZjGk4xjTfMR_9oo7uCo8H13IOajHJuF_BasL-1sK6meBw1qfyDvo_MZw/exec',
-  APP_NAME: 'TerraLote',
-  CACHE_VERSION: '2.0.0'
-});
